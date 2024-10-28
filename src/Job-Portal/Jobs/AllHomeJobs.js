fa2934756a7cac4e9a3394dd61b7910c0c3dc9a2
parvaiz1 <97357523+parvaiz1@users.noreply.github.com>
2024-10-28 15:20:16 +0530

    commit

--- src/Job-Portal/Jobs/AllHomeJobs.js
+++ src/Job-Portal/Jobs/AllHomeJobs.js
@@ -46,12 +46,43 @@ function Home() {
   const [Active, setActive] = useState("")
   const screenSize = useScreenSize();
 
-  let AgeTags = [
-    { value: '2 to 5 Yrs', label: '2 to 5 Yrs ' }, { value: '6 to 10 Yrs', label: '6 to 10 Yrs' },
-    { value: '11 to 15', label: '11 to 15' }, { value: '16 and above', label: '16 and above' }
-  ]
+  // let AgeTags = [
+ 
+  let jobTags = [
+    { value: 'TECHNOLOGIES', label: 'TECHNOLOGIES'},
 
-  let TechTags = [
+    { value: 'Angular js', label: 'Angular js' }, { value: 'Vue js', label: 'Vue js' },
+    { value: 'NextJs', label: 'NextJs' },{ value: '.NET', label: '.NET' }, { value: 'Larvel', label: 'Larvel' },
+    { value: 'Kotlin', label: 'Kotlin' }, { value: 'Android', label: 'Android' }, { value: 'iOS', label: 'iOS' },
+    { value: 'Xamarin', label: 'Xamarin' }, { value: 'Ember JS', label: 'Ember JS' }, 
+    { value: 'Cordova', label: 'Cordova' }, { value: 'Groovey,Grails', label: 'Groovey,Grails' },
+    { value: 'QT', label: 'QT' }, { value: 'Oracle', label: 'Oracle' }, { value: 'Postgres SQL', label: 'Postgres SQL' },
+    { value: 'CouchDB', label: 'CouchDB' }, { value: 'Redis', label: 'Redis' }, { value: 'Azure', label: 'Azure' },
+    { value: 'AWS', label: 'AWS' }, { value: 'Rackspace', label: 'Rackspace' }, { value: 'Heroku', label: 'Heroku' },
+    { value: 'GoogleCloud', label: 'GoogleCloud' }, { value: 'Weblogic', label: 'Weblogic' },
+    { value: 'Apache', label: 'Apache' }, { value: 'Lotus', label: 'Lotus' }, { value: 'Domino', label: 'Domino' },
+    { value: 'MS IIS', label: 'MS IIS' }, { value: 'ColdFusion', label: 'ColdFusion' },
+    { value: 'nginx', label: 'nginx' }, { value: 'Resin', label: 'Resin' }, { value: 'Selenium', label: 'Selenium' },
+    { value: 'HP LoadRunner', label: 'HP LoadRunner' }, { value: 'jUnit', label: 'jUnit' },
+    { value: 'Jira', label: 'Jira' }, { value: 'Confluence', label: 'Confluence' },
+    { value: 'Testrails', label: 'Testrails' },{ value: 'Machine Learing', label: 'Machine Learing' },
+    { value: 'Cybersecurity', label: 'Cybersecurity' }, { value: 'AI', label: 'AI' },
+    { value: 'Backend', label: 'Backend' }, { value: 'Frontend', label: 'Frontend' },
+    { value: 'HTML-CSS', label: 'HTML-CSS' }, { value: 'MongoDB', label: 'MongoDB' },
+    { value: 'MySql', label: 'MySql' }, { value: 'Flutter', label: 'Flutter' },
+    { value: 'Mobile App Developer', label: 'Mobile App Developer' }, { value: 'Artificial Intelligence', label: 'Artificial Intelligence' },
+    { value: 'React Native', label: 'React Native' }, { value: 'DevOps Engineer', label: 'DevOps Engineer' },
+    { value: 'Security developer', label: 'Security developer' }, { value: 'Data science', label: 'Data science' },
+    { value: 'Data Analyst', label: 'Data Analyst' }, { value: 'Game Developer', label: 'Game Developer' },
+    { value: 'Graphic Developers', label: 'Graphic Developers' }, { value: 'AI Engineer', label: 'AI Engineer' },
+    { value: 'Security Developer', label: 'Security Developer' }, { value: 'Cloud Developers', label: 'Cloud Developers' },
+
+
+  // ]
+
+  // let TechTags = [
+    { value: 'ROLE', label: 'ROLE'},
+
     { value: 'S-w Developer', label: 'S/w Developer ' }, { value: 'Firmware Tester', label: 'Firmware Tester' },
     { value: 'Firmware Developer', label: 'Firmware Developer' }, { value: 'Hardware Engineer', label: 'Hardware Engineer' },
     { value: 'Agile', label: 'Agile' }, { value: 'Scrum Master', label: "Scrum Master" },
@@ -64,36 +95,112 @@ function Home() {
     { value: 'QA Manager', label: 'QA Manager' }, { value: 'Verification Lead', label: 'Verification Lead' },
     { value: 'Validation Engineer', label: 'Validation Engineer' },
     { value: 'Testing Manager', label: 'Testing Manager' }, { value: 'Team Lead', label: 'Team Lead' },
-    { value: 'Senior Developer', label: 'Senior Developer' },
-  ]
+    { value: 'Senior Developer', label: 'Senior Developer' }, { value: 'Admin', label: 'Admin' },    
+    { value: 'Purchase', label: 'Purchase' },{ value: 'Finance', label: 'Finance' },   
+    { value: 'Recruiter', label: 'Recruiter' }, { value: 'Lab Technician', label: 'Lab Technician' },
+    { value: 'Store keeper', label: 'Store keeper' },
+  // ]
 
-  let jobTags = [
-    { value: 'Angular js', label: 'Angular js' }, { value: 'Vue js', label: 'Vue js' },
-    { value: 'NextJs', label: 'NextJs' }, { value: '.NET', label: '.NET' }, { value: 'Larvel', label: 'Larvel' },
-    { value: 'Kotlin', label: 'Kotlin' }, { value: 'Android', label: 'Android' }, { value: 'iOS', label: 'iOS' },
-    { value: 'Xamarin', label: 'Xamarin' }, { value: 'Ember JS', label: 'Ember JS' }, { value: 'Cordova', label: 'Cordova' },
-    { value: 'Groovey,Grails', label: 'Groovey,Grails' }, { value: 'QT', label: 'QT' },
-    { value: 'Oracle', label: 'Oracle' }, { value: 'Postgres SQL', label: 'Postgres SQL' },
-    { value: 'CouchDB', label: 'CouchDB' }, { value: 'Redis', label: 'Redis' }, { value: 'Azure', label: 'Azure' },
-    { value: 'AWS', label: 'AWS' }, { value: 'Rackspace', label: 'Rackspace' }, { value: 'Heroku', label: 'Heroku' },
-    { value: 'GoogleCloud', label: 'GoogleCloud' }, { value: 'Weblogic', label: 'Weblogic' },
-    { value: 'Apache', label: 'Apache' }, { value: 'Lotus', label: 'Lotus' }, { value: 'Domino', label: 'Domino' },
-    { value: 'MS IIS', label: 'MS IIS' }, { value: 'ColdFusion', label: 'ColdFusion' },
-    { value: 'nginx', label: 'nginx' }, { value: 'Resin', label: 'Resin' }, { value: 'Selenium', label: 'Selenium' },
-    { value: 'HP LoadRunner', label: 'HP LoadRunner' }, { value: 'jUnit', label: 'jUnit' },
-    { value: 'Jira', label: 'Jira' }, { value: 'Confluence', label: 'Confluence' },
-    { value: 'Testrails', label: 'Testrails' }, { value: 'PLC,SCADA', label: 'PLC,SCADA' },
-    { value: 'ModBUS', label: 'ModBUS' }, { value: 'CANBUS', label: 'CANBUS' }, { value: 'Machine Learing', label: 'Machine Learing' },
-    { value: 'Cybersecurity', label: 'Cybersecurity' }, { value: 'AI', label: 'AI' },
-    { value: 'Backend', label: 'Backend' }, { value: 'Frontend', label: 'Frontend' },
-    { value: 'HTML-CSS', label: 'HTML-CSS' }, { value: 'MongoDB', label: 'MongoDB' },
-    { value: 'MySql', label: 'MySql' }, { value: 'Flutter', label: 'Flutter' },
-    { value: 'Mobile App Developer', label: 'Mobile App Developer' }, { value: 'Artificial Intelligence', label: 'Artificial Intelligence' },
-    { value: 'React Native', label: 'React Native' }, { value: 'DevOps Engineer', label: 'DevOps Engineer' },
-    { value: 'Security developer', label: 'Security developer' }, { value: 'Data science', label: 'Data science' },
-    { value: 'Data Analyst', label: 'Data Analyst' }, { value: 'Game Developer', label: 'Game Developer' },
-    { value: 'Graphic Developers', label: 'Graphic Developers' }, { value: 'AI Engineer', label: 'AI Engineer' },
-    { value: 'Security Developer', label: 'Security Developer' }, { value: 'Cloud Developers', label: 'Cloud Developers' },
+  // let TOOLSPROTOCOLS =[
+    { value: 'TOOLS/PROTOCOLS', label: 'TOOLS/PROTOCOLS'},
+
+    { value: 'HP LoadRunner', label: 'HP LoadRunner' },
+    { value: 'jUnit', label: 'jUnit' },
+    { value: 'Jira', label: 'Jira' },
+    { value: 'Confluence', label: 'Confluence' },
+    { value: 'Testrails', label: 'Testrails' },
+    { value: 'PLC-SCADA', label: 'PLC-SCADA' },
+    { value: 'ModBUS', label: 'ModBUS' },
+    { value: 'CAN BUS', label: 'CAN BUS' },
+  // ]
+  // let INDUSTRY=[
+    { value: 'INDUSTRY', label: 'INDUSTRY' },
+
+    { value: 'Aerospace', label: 'Aerospace' },
+    { value: 'IT', label: 'IT' },
+    { value: 'Banking', label: 'Banking' },
+    { value: 'Healthcare', label: 'Healthcare' },
+    { value: 'FMCG', label: 'FMCG' },
+    { value: 'Telecommunication', label: 'Telecommunication' },
+    { value: 'Multimedia', label: 'Multimedia' },
+    { value: 'Gaming', label: 'Gaming' },
+    { value: 'Defence', label: 'Defence' },
+    { value: 'Power', label: 'Power' },
+    { value: 'Security', label: 'Security' },
+    { value: 'Education', label: 'Education' },
+  // ]
+  // let JOB_TYPE=[
+    { value: 'Job Type', label: 'Job Type'},
+
+    { value: 'Full Time', label: 'Full Time' },
+    { value: 'Contract', label: 'Contract' },
+    { value: 'Part Time', label: 'Part Time' },
+    { value: 'Work from Home', label: 'Work from Home' },
+    { value: 'Remote', label: 'Remote' },
+    { value: 'Freelancer', label: 'Freelancer' },
+    { value: 'On-Site', label: 'On-Site' },
+    { value: 'International Relocation', label: 'International Relocation' },
+    { value: 'Frequent Traveling', label: 'Frequent Traveling' },
+    { value: 'Temporary Assignment', label: 'Temporary Assignment' },
+  // ]
+  // let Experiance
+  { value: 'EXPERIENCE', label: 'EXPERIENCE'},
+
+  { value: '2 to 5 Yrs', label: '2 to 5 Yrs ' }, { value: '6 to 10 Yrs', label: '6 to 10 Yrs' },
+  // let SALARY=[ 
+    { value: 'SALARY', label: 'SALARY'},
+
+    { value: '11 to 15', label: '11 to 15' }, { value: '16 and above', label: '16 and above' },
+    { value: '<10L', label: '<10L' },
+    { value: '10 to 20L', label: '10 to 20L' },
+    { value: '20 to 30L', label: '20 to 30L' },
+    { value: '30 and above', label: '30 and above' },
+  // ]
+
+  // let NOTICE_PERIOD=[
+    { value: 'NOTICE PERIOD', label: 'NOTICE PERIOD'},
+
+    { value: 'Immediate', label: 'Immediate' },
+    { value: '30days', label: '30days' },
+    { value: '60days', label: '60days' },
+  // ]
+
+  // let COMPANY_TYPE=[
+    { value: 'COMPANY TYPE', label: 'COMPANY TYPE' },
+
+    { value: 'Fortune500', label: 'Fortune500' },
+    { value: 'MNC', label: 'MNC' },
+    { value: 'Indian MNC', label: 'Indian MNC' },
+    { value: 'Product Based', label: 'Product Based' },
+    { value: 'Consulting', label: 'Consulting' },
+    { value: 'Manpower sourcing', label: 'Manpower sourcing' },
+    { value: 'Firm', label: 'Firm' },
+    { value: 'Pvt Ltd', label: 'Pvt Ltd' },
+  // ]
+  // let COLLEGE_TYPE=[
+    { value: 'COLLEGE TYPE', label: 'COLLEGE TYPE' },
+
+    { value: 'IITs/NITs', label: 'IITs/NITs' },
+    { value: 'Top 100', label: 'Top 100' },
+    { value: 'Top 500', label: 'Top 500' },
+    { value: 'Others', label: 'Others' },
+  // ]
+
+  // let EDUCATION =[
+    { value: 'EDUCATION', label: 'EDUCATION' },
+
+    { value: 'Phd', label: 'Phd' },
+    { value: 'ME/ MTech', label: 'ME/ MTech' },
+    { value: 'MBA', label: 'MBA' },
+    { value: 'BE/BTECH', label: 'BE/BTECH' },
+    { value: 'Diploma', label: 'Diploma' },
+    { value: 'MCA/BCA', label: 'MCA/BCA' },
+    { value: 'Bcom/BA', label: 'Bcom/BA' },
+    { value: 'Others', label: 'Others' },
+
+  // ]
+
+  // let jobTags = [
   ]
 
   let JobLocationTags = ["Bangalore", "Hyderabad", "Chennai", "Mumbai"]
@@ -471,63 +578,28 @@ function Home() {
                   <button className={location == "Chennai" || location == "Hyderabad" || location == "Mumbai" ?
                     styles.disable : Active === "Bangalore" ? styles.locationActive : styles.JobLocationFilter} disabled={location == "Chennai" ||
                       location == "Hyderabad" || location == "Mumbai" || location == "Delhi"} name="filter" onClick={() => { getLocation(location.toLowerCase()); setActive("Bangalore") }} >{location}</button>
-
                 )
               })
             }
           </div><br></br>
-
-          <div style={{display:"flex" , alignItems:"center"}}>
-
-          <p>
-Technologies
-          </p>
 
           <div className={styles.JobtitleFilterWrapper}>
             <buton className={Active === "All" ? styles.active : styles.JobtitleFilter} onClick={() => { getjobs(); setActive("All") }}>All</buton>
             {
               jobTags.map((tags, i) => {
                 return (
-                  <buton className={Active === tags.value ? styles.active : styles.JobtitleFilter} onClick={() => { filterByJobTitle(tags.value) }}>{tags.value} </buton>
-                )
+                                   
+                  <button disabled={tags.value==="TECHNOLOGIES" || tags.value==="EDUCATION" || tags.value==="COLLEGE TYPE" || tags.value==="NOTICE PERIOD" || tags.value==="SALARY" || 
+                    tags.value==="EXPERIENCE" || tags.value==="Job Type" || tags.value==="INDUSTRY" || tags.value==="TOOLS/PROTOCOLS" || tags.value==="ROLE" } 
+                    className={tags.value==="TECHNOLOGIES" || tags.value==="EDUCATION" || tags.value==="COLLEGE TYPE" || tags.value==="NOTICE PERIOD" || tags.value==="SALARY" || 
+                    tags.value==="EXPERIENCE" || tags.value==="Job Type" || tags.value==="INDUSTRY" || tags.value==="TOOLS/PROTOCOLS" || tags.value==="ROLE"?
+                    styles.TagHeading:  Active === tags.value ? 
+                    styles.active : styles.JobtitleFilter} onClick={() => { filterByJobTitle(tags.value) }}>{tags.value} </button>
+                
+                  )
               })
             }
           </div>
-          </div>
-          <div style={{display:"flex" , alignItems:"center"}}>
-
-            <p style={{marginLeft:"1%"}}>Role</p>          
-
-          <div className={styles.JobtitleFilterWrapper} style={{marginLeft:"2.4%"}}>
-            <buton className={Active === "All" ? styles.active : styles.JobtitleFilter} onClick={() => { getjobs(); setActive("All") }}>All</buton>
-            {
-              TechTags.map((tags, i) => {
-                return (
-                  <buton className={Active === tags.value ? styles.active : styles.JobtitleFilter} onClick={() => { filterByJobTitle(tags.value) }}>{tags.value} </buton>
-                )
-              })
-            }
-          </div>
-          </div>
-
-          <div style={{display:"flex" , alignItems:"center"}}>
-
-          <p>Experience</p>          
-
-          <div className={styles.JobtitleFilterWrapper} style={{marginLeft:"1%"}}>
-            <buton className={Active === "All" ? styles.active : styles.JobtitleFilter} onClick={() => { getjobs(); setActive("All") }}>All</buton>
-            {
-              AgeTags.map((tags, i) => {
-                return (
-                  // <buton className={Active === tags.value ? styles.active : styles.JobtitleFilter} onClick={() => 
-                  //   { filterByJobTitle(tags.value) }}>{tags.value} </buton>
-                  <buton className={Active === tags.value ? styles.active : styles.JobtitleFilter} onClick={() => { filterByJobTitle(tags.value) }}>{tags.value} </buton>
-                )
-              })
-            }
-          </div>
-          </div>
-
 
           <div style={{ display: "flex", justifyContent: "space-between" }}>
             {nopageFilter ?
