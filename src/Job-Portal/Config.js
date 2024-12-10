55cb3047b8c49b5258a70210290a5b7065b6f73c
parvaiz1 <97357523+parvaiz1@users.noreply.github.com>
2024-12-10 12:26:49 +0530

    commit

--- src/Job-Portal/Config.js
+++ src/Job-Portal/Config.js
@@ -4,7 +4,8 @@ export const msalConfig = {
   auth: {
     clientId: "097b08ff-185e-4153-aedc-0e5814e0570c",  // Replace with your Application (client) ID
     authority: "https://login.microsoftonline.com/ae4ae520-4db7-4149-ad51-778e540d8bec",  // Replace with your Tenant ID
-    redirectUri: "http://localhost:80",  // Your app's redirect URI
+    // redirectUri: "http://localhost:80",  // Your app's redirect URI
+    redirectUri: "https://www.itwalkin.com",  // Your app's redirect URI
   },
   cache: {
     cacheLocation: "localStorage", // Options: sessionStorage, localStorage
