09d3fa7f6c003fcd4a988b37c157a2f72e918b3b
parvaiz1 <97357523+parvaiz1@users.noreply.github.com>
2025-01-27 23:07:01 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -89,12 +89,12 @@ import socketIO from 'socket.io-client';
 import SidebarNav from "./Job-Portal/BigSideNav";
 import useScreenSize from '../src/Job-Portal/SizeHook';
 import AskQuestion from "./Job-Portal/PostJobs/postQuesion";
-axios.defaults.baseURL = "http://localhost:8080" 
+// axios.defaults.baseURL = "http://localhost:8080" 
 // axios.defaults.baseURL = "https://i-twalkin-backend-testrelease-2-0-1-0824-zdnu.vercel.app" // new vercel
 
 // axios.defaults.baseURL = "https://i-twalkin-backend-testrelease-2-0-1-0824.vercel.app"  // vercel Test
 
-// axios.defaults.baseURL = "https://itwalkin-backend-testrelease-2-0-1-0824-ns0g.onrender.com" // Render Test
+axios.defaults.baseURL = "https://itwalkin-backend-testrelease-2-0-1-0824-ns0g.onrender.com" // Render Test
 
 // Vercel Main Live 
 //  axios.defaults.baseURL = "https://itwalkin-backend-testrelease-2-0-1-0824.onrender.com";
